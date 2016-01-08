ffc65717ecb7a7d9a0f2d54c85767c7937c61e7d
Deni Husni FR <denifahri@positron>
2016-01-08 15:13:23 +0700

    Initialization Web Core

---
+++ dhfr-web-core/src/main/java/com/dhfr/webcore/WebCoreConstant.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dhfr.webcore;
+
+/**
+ *
+ * @author Deni Husni FR
+ */
+public class WebCoreConstant {
+
+    public static final String SAVE_CONDITION = "SAVE_CONDITION";
+    public static final String BAHASA_ACTIVE = "bahasa_active";
+    public static final String UPDATE_CONDITION = "UPDATE_CONDITION";
+    public static final String LICENSE_EVERY_YEAR = "LICENSE_EVERY_YEAR";
+    public static final String LICENSE_FOR_EVER = "LICENSE_FOR_EVER";
+    public static final String LICENSE_TRIAL = "LICENSE_TRIAL";
+    public static final byte[] KEYVALUE_LICENSE = new byte[]{'a', 'i', 's', 'A', 'H', 'Z', 'B', 'y', 'a', 'L', 'N', 'L', 'd', 'h', 'f', 'r'};
+    public static final String AES_ALGO_LICENSE = "AES";
+    public static final String DATE_LICENSE_PATTERN = "dd MMMM yyyy";
+    public static final String LICENSE_ACTIVE = "LICENSE_ACTIVE";
+    public static final String LICENSE_PASS = "LICENSE_PASS";
+    public static final String IIT_OPTIMAHR = "IIT_OPTIMAHR";
+
+}
